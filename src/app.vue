f7e9ac377af823cc4b01efc42e89d44ff60b0db3
Anderson Tomazeto <packetspy@gmail.com>
2021-01-04 14:58:42 -0300

    Tela de login

--- src/app.vue
+++ src/app.vue
@@ -29,62 +29,17 @@ export default {
 
 // Normalize default styles across browsers,
 // https://necolas.github.io/normalize.css/
-@import '~normalize.css/normalize.css';
+// @import '~normalize.css/normalize.css';
+
 // Style loading bar between pages.
 // https://github.com/rstacruz/nprogress
-@import '~nprogress/nprogress.css';
+// @import '~nprogress/nprogress.css';
 
 // Design variables and utilities from src/design.
-@import '@design';
+// @import '@design';
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-}
+@import '~bootstrap-vue/dist/bootstrap-vue.css';
+@import '~perfect-scrollbar/css/perfect-scrollbar.css';
 
-body {
-  background: $color-body-bg;
-}
-#app {
-  @extend %typography-small;
-}
-
-// ===
-// Base element styles
-// ===
-
-a,
-a:visited {
-  color: $color-link-text;
-}
-
-h1 {
-  @extend %typography-xxlarge;
-}
-
-h2 {
-  @extend %typography-xlarge;
-}
-
-h3 {
-  @extend %typography-large;
-}
-
-h4 {
-  @extend %typography-medium;
-}
-
-h5,
-h6 {
-  @extend %typography-small;
-}
-
-// ===
-// Vendor
-// ===
-
-#nprogress .bar {
-  background: $color-link-text;
-}
+@import '@assets/sass/style.vue';
 </style>
